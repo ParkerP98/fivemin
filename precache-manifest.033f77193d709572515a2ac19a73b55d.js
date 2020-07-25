a658f82cca9ef08bd3f0c2e483f53d06e162047c
Bigdady02 <Parker.prowell@gmail.com>
2020-07-25 16:11:46 -0700

    Updates

---
+++ precache-manifest.033f77193d709572515a2ac19a73b55d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2dc71768eee3529b9d7274bc9a5749",
+    "url": "/fivemin/index.html"
+  },
+  {
+    "revision": "24b67c31d3ba3ecc318b",
+    "url": "/fivemin/static/css/main.383f8b0f.chunk.css"
+  },
+  {
+    "revision": "98957c31571beed8323a",
+    "url": "/fivemin/static/js/2.cd7cd9c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fivemin/static/js/2.cd7cd9c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24b67c31d3ba3ecc318b",
+    "url": "/fivemin/static/js/main.0ee80734.chunk.js"
+  },
+  {
+    "revision": "dcc887e684006e587ef7",
+    "url": "/fivemin/static/js/runtime-main.460f901a.js"
+  },
+  {
+    "revision": "d012db18bed53ce7aec754c10c72d4fd",
+    "url": "/fivemin/static/media/wideskyad.d012db18.png"
+  },
+  {
+    "revision": "df42362df02f637e68baa18f208b788a",
+    "url": "/fivemin/static/media/wideskyad1.df42362d.png"
+  }
+]);
